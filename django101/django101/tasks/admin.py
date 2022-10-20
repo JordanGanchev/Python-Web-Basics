4cfaa5e2b0760801ef2d7607561eff1630153ce2
Jordan Ganchev <jordan.ganchev.1434@gmail.com>
2022-10-20 10:59:11 +0300

    exercise

--- django101/django101/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from django101.tasks.models import Task
-
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'priority')
